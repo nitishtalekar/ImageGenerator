e59fce7b8754135f5aaab961af2d32915d40593b
Nitish <nitishtalekar.nt503@gmail.com>
2023-12-28 01:50:54 -0600

    update demo with DB

--- demo.py
+++ demo.py
@@ -4,6 +4,65 @@ import requests
 import io
 from PIL import Image
 
+import psycopg2
+from psycopg2 import sql
+
+import boto3
+from botocore.exceptions import NoCredentialsError
+import random
+
+def generate_filename(user_input):
+    stripped_input = user_input.replace(" ", "")
+    random_int1 = random.randint(1, 1000)
+    random_int2 = random.randint(1, 1000)
+    result = f"{random_int1}{stripped_input}{random_int2}.jpg"
+    return result
+
+def upload_image_to_s3(image_bytes, file):
+    bucket_name = 'streamlit-demo-bucket'
+    s3_file_key = file
+    aws_access_key_id = 'AKIAY2KQUWZZ6EZ2AD5P'
+    aws_secret_access_key = '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'
+    try:
+        s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        s3.upload_fileobj(io.BytesIO(image_bytes), bucket_name, s3_file_key)
+        s3_url = f"https://{bucket_name}.s3.amazonaws.com/{s3_file_key}"
+        print("Data uploaded successfully!")
+        return s3_url
+    except NoCredentialsError:
+        print("Credentials not available")
+
+
+def add_data(prompt, url):
+    try:
+        connection = psycopg2.connect(
+            host="streamlitdemo.cz08a084ewcv.us-west-2.rds.amazonaws.com",
+            database="streamlit_image_data",
+            user="streamlit_admin",
+            password="Streamlit123"
+        )
+        cursor = connection.cursor()
+
+        insert_query = sql.SQL("""
+            INSERT INTO image_data (prompt, image)
+            VALUES (%s, %s);
+        """)
+
+        cursor.execute(insert_query, (prompt, url))
+
+        connection.commit()
+
+        print("Data added successfully!")
+
+    except Exception as e:
+        print("Error:", e)
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
 API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
 headers = {"Authorization": "Bearer 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"}
 
@@ -16,29 +75,25 @@ def getImage(input):
     	"inputs": input,
     })
     return image_bytes
+    
 
-# Define the main function for the Streamlit app
 def main():
-    # Set the page title
+
     st.title("StreamLit Demo")
 
-    # Add a space for layout purposes
     st.write(" ")
 
-    # Create a centered textbox for user input
-    user_input = st.text_input("Enter prompt here:", key="user_input", value="")
+    user_input = st.text_input("Enter propmt here:", key="user_input", value="")
 
-    # Add a space for layout purposes
     st.write(" ")
 
-    # Create a centered button
     button_clicked = st.button("Generate")
-
-    # Display the input as a heading when the button is clicked
     if button_clicked:
         st.header(f"You entered: {user_input}")
         image_bytes = getImage(user_input)
         image = Image.open(io.BytesIO(image_bytes))
+        url = upload_image_to_s3(image_bytes, generate_filename(user_input))
+        add_data(user_input, url)
         st.image(image, caption=user_input, use_column_width=True)
 
 # Run the app
